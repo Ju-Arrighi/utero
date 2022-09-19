e07168a1c8ed3fbf7c0671343b73d7be8a0a6bbb
Juliana Arrighi <jarrighi@poli.ufrj.br>
2022-09-19 17:07:27 -0300

    dart and webpack

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -70,3 +70,20 @@ module.exports = {
     new MiniCssExtractPlugin(),
   ],
 };
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
